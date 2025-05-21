9a35202f427003ec8c69530f2eb65cb917d3f17e
vrotondo <vrfr91@gmail.com>
2025-05-21 18:10:02 -0400

    Student commits

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,40 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    
+    if earthquake:
+        earthquake_data = {
+            "id": earthquake.id,
+            "magnitude": earthquake.magnitude,
+            "location": earthquake.location,
+            "year": earthquake.year
+        }
+        return make_response(earthquake_data, 200)
+    else:
+        return make_response({"message": f"Earthquake {id} not found."}, 404)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    earthquake_list = []
+    for quake in earthquakes:
+        earthquake_list.append({
+            "id": quake.id,
+            "magnitude": quake.magnitude,
+            "location": quake.location,
+            "year": quake.year
+        })
+    
+    response_data = {
+        "count": len(earthquake_list),
+        "quakes": earthquake_list
+    }
+    
+    return make_response(response_data, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
